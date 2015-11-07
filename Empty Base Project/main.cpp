60a94a0e42e95a76a76d6cf5bb2260ffde10910f
Frahaan Hussain <Sonar Systems>
2015-11-07 11:35:53 +0000

    Empty Base Project

---
+++ Empty Base Project/main.cpp
@@ -0,0 +1,44 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main( void )
+{
+    GLFWwindow *window;
+    
+    // Initialize the library
+    if ( !glfwInit( ) )
+    {
+        return -1;
+    }
+    
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
+    
+    if ( !window )
+    {
+        glfwTerminate( );
+        return -1;
+    }
+    
+    // Make the window's context current
+    glfwMakeContextCurrent( window );
+    
+    /* Loop until the user closes the window */
+    while ( !glfwWindowShouldClose( window ) )
+    {
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        // Render OpenGL here
+ 
+        
+        // Swap front and back buffers
+        glfwSwapBuffers( window );
+        
+        // Poll for and process events
+        glfwPollEvents( );
+    }
+    
+    glfwTerminate( );
+    
+    return 0;
+}
