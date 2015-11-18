c920a227fc180edbd27874131b148db98ff74d51
Frahaan Hussain <Sonar Systems>
2015-11-18 15:54:40 +0000

    Sample Code For Triangle Strip and Triangle Fan

---
+++ Drawing A Triangle Fan - GL_TRIANGLE_FAN/main.cpp
@@ -0,0 +1,67 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main( void )
+{
+    GLFWwindow *window;
+    
+    // Initialize the library
+    if ( !glfwInit( ) )
+    {
+        return -1;
+    }
+    
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow( SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL );
+    
+    if ( !window )
+    {
+        glfwTerminate( );
+        return -1;
+    }
+    
+    // Make the window's context current
+    glfwMakeContextCurrent( window );
+    
+    glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+    glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+    glOrtho( 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1 ); // essentially set coordinate system
+    glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+    glLoadIdentity( ); // same as above comment
+    
+    GLfloat polygonVertices[] =
+    {
+        320, 240, 0,
+        370, 290, 0,
+        420, 240, 0,
+        370, 190, 0
+    };
+    
+    glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+    
+    // Loop until the user closes the window
+    while ( !glfwWindowShouldClose( window ) )
+    {
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // render OpenGL here
+        glEnableClientState( GL_VERTEX_ARRAY );
+        glVertexPointer( 3, GL_FLOAT, 0, polygonVertices );
+        glDrawArrays( GL_TRIANGLE_FAN, 0, 4 );
+        glDisableClientState( GL_VERTEX_ARRAY );
+        
+        // Swap front and back buffers
+        glfwSwapBuffers( window );
+        
+        // Poll for and process events
+        glfwPollEvents( );
+    }
+    
+    glfwTerminate( );
+    
+    return 0;
+}
